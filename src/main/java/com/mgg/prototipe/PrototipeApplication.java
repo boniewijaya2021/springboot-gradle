27c0500f13696a959ba7d1cc83b978a077405e29
Set up later <setuplater@Sets-MacBook-Pro.local>
2025-01-29 10:25:34 +0700

    satu

---
+++ src/main/java/com/mgg/prototipe/PrototipeApplication.java
@@ -0,0 +1,13 @@
+package com.mgg.prototipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototipeApplication.class, args);
+	}
+
+}
